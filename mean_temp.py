e6f3fd9f53a39847ff24441f4f10e378543ad103
Shawn Hutchinson <shutch@ksu.edu>
2023-08-15 10:33:25 -0500

    Create mean_temp.py

---
+++ mean_temp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    File name: mean_temp.py
+    Author: Shawn Hutchinson
+    Description:  Python script that calculates mean daily temperature from two user defined variables representing a daily high (x) and low (y) temperature
+    Date created: 08/15/2023
+    Date last modified: 08/15/2023
+    Python Version: 3.9.16
+"""
+
+# Assign variable x the value for the previous day's high temperature
+x = 
+
+# Assign variable y the value for the previous day's low temperature
+y =  
+
+# Create variable z and use mathematical operators to calculate the mean temperature
+z = 
+
+# Use this custom statement that prints the result to screen
+print("Yesterday's mean daily temperature was {0} degrees.".format(z))
